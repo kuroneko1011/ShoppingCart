6251c1972b847fbe01dc7ea74c7ba7ad9f3ac472
Trong Nhan <nhantrongho2002@gmail.com>
2022-02-07 22:51:02 +0700

    with route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage.component";
 
-import HomePage from './pages/homepage.component';
+import "./App.css";
 
-import './App.css';
-import Directory from './component/directory/directory.component';
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <Directory />
+      // <div className='App'>
+      //   <HomePage/>
+      // </div>
+      <div>
+        <Switch>
+        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+        </Switch>
       </div>
     );
   }
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,42 +1,10 @@
 import React from 'react';
 import './homepage.style.scss';
+import Directory from '../component/directory/directory.component';
 
 const HomePage = () => (
     <div className="homepage">
-        <h1>Welcome to my homepage</h1>
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <div className='title'>HATS</div>
-                    <div className='subtitle'>SHOP NOW</div>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <div className='title'>JACKETS</div>
-                    <div className='subtitle'>SHOP NOW</div>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <div className='title'>SHOES</div>
-                    <div className='subtitle'>SHOP NOW</div>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <div className='title'>WOMEN</div>
-                    <div className='subtitle'>SHOP NOW</div>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <div className='title'>MENS</div>
-                    <div className='subtitle'>SHOP NOW</div>
-                </div>
-            </div>
-        </div>
+        <Directory/>
     </div>
 ) 
-
 export default HomePage;
