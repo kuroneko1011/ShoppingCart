c30690c112cf081d47bbc4b8088cd08a34659f40
Trong Nhan <nhantrongho2002@gmail.com>
2022-02-18 20:49:39 +0700

    Refactor code with reselect in cart dropdown and cart-icon quantity

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.style.scss";
+import { selectCartItems } from "../../redux/cart/cart.selector";
 
 import { connect } from "react-redux";
 
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps, null)(CartDropdown);
